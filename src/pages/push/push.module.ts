ce44665aeef394a0292d20e954d1d7ef5f6e151c
Peter <pvroom@convergence-us.com>
2019-02-04 04:23:01 -0500

    added appflow sdk

---
+++ src/pages/push/push.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PushPage } from './push';
+
+@NgModule({
+  declarations: [
+    PushPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PushPage),
+  ],
+})
+export class PushPageModule {}
